0348ef5cca4c3ad28ecd0d44438ce571969da9b1
datadigitalshredder <hov21003@byui.edu>
2022-01-27 08:53:50 +0200

    Week 3 & 4 day 3

--- hilo_game/the_game/director.py
+++ hilo_game/the_game/director.py
@@ -5,9 +5,8 @@ class Director:
     """A person who directs the game. 
     
     The responsibility of a Director is to control the sequence of play.
-
     Attributes:
-        dice (List[card]): A list of the card instance.
+        card (List[card]): A list of the card instance.
         is_playing (boolean): Whether or not the game is being played.
         score (int): The score for one round of play.
         total_score (int): The score for the entire game.
@@ -19,9 +18,14 @@ class Director:
         Args:
             self (Director): an instance of Director.
         """
+        self.card = []
         self.is_playing = True
-        self.score = 300
+        self.score = 0
         self.total_score = 300
+
+        for i in range(13):
+            card = Card()
+            self.card.append(card)
 
     def start_game(self):
         """Starts the game by running the main game loop.
@@ -34,11 +38,10 @@ class Director:
             self.do_updates()
             self.do_outputs()
         else:
-            print('Game Over!')
+            print('GAME OVER!')
 
     def get_inputs(self):
         """Ask the user if they want to play.
-
         Args:
             self (Director): An instance of Director.
         """
@@ -47,24 +50,24 @@ class Director:
 
     def do_updates(self):
         """Updates the player's score.
-
         Args:
             self (Director): An instance of Director.
         """
         if not self.is_playing:
             return 
 
-        number = random.randint(1, 13) # Generate a random card number from 1 to 13
-        cards = int(input('Guess, is the next card higher or lower? [1 - 13] ')) # Player guesses the next card
-        print(f'The card is: {number}!')
-        if cards > number:
+        card_guess = int(input('Guess the next card number [1 - 13] ')) # Player guesses the next card
+        next_card = random.randint(1, 13) # Generate a random card number from 1 to 13
+        
+        print(f'The next card is: {next_card}!')
+        if card_guess > next_card:
             print('You guessed higher, your score is now 100 points more')
-            cards = 100
+            score = 100
         else:
             print('You guessed lower, your score is now 75 points less')
-            cards = -75
+            score = -75
       
-        self.total_score += cards
+        self.total_score += score
 
         # Terminate the game when score is zero
         if self.total_score == 0:
@@ -72,8 +75,7 @@ class Director:
             return
             
     def do_outputs(self):
-        """Displays the dice and the score. Also asks the player if they want to play again if they their score is > zero. 
-
+        """Displays the score. Also checks if the player is eligible to play again, if they their score is > zero. 
         Args:
             self (Director): An instance of Director.
         """
@@ -82,4 +84,3 @@ class Director:
         
         print(f"Total score is: {self.total_score}\n")
         self.is_playing == (self.score > 0)
-       
--- hilo_game/the_game/hilo.py
+++ hilo_game/the_game/hilo.py
@@ -1,19 +1,27 @@
-
+# import random
 class Card:
     
     def __init__(self):
         self.value = 0
         self.points = 300
+        self.guess = 0
     
     # TO THIS POINT THE GAME IS WORKING, NOT SURE WHAT TO DO WITH THE BELOW CODE OR IT'S NOT NEEDED.
 
     # def play(self):
-    #     # card_guess = int(input('Is your guess higher or lower? [1 - 13] '))
+    #     self.guess = int(input('Is your guess higher or lower? [1 - 13] '))
     #     self.value = random.randint(1, 14)
+
+
+    #     print(f'The card is: {self.guess}!') ##
        
-    #     if self.value > self.points:
+    #     if self.guess > self.value:
     #         # guess = True
     #         # if guess:
-    #         self.points = 100 
+    #         print('You guessed higher, your score is now 100 points more') ##
+    #         self.points += 100 
     #     else:
-    #         self.points = -75
+    #         print('You guessed lower, your score is now 75 points less')
+    #         self.points -= 75
+
+    #     # self.total_score > 0
